636fbc5c40a4ad4829aa692f306ed8de8257165a
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-12 22:21:15 +0300

    Add some services to check validity of inputs and mappings.

--- whisper-front/src/services/chatservice/chatParams.js
+++ whisper-front/src/services/chatservice/chatParams.js
@@ -1,3 +1,3 @@
 
 // Define the required parameters for a valid chat
-export const requiredChatParams = ["name", "profile_pic", "message", "message_state", "muted",  "time", "last_seen", "unread_notifications"]
+export const requiredChatParams = ["name", "profile_pic", "message", "message_state", "muted", "last_seen", "unread_notifications", "message_time", "message_type"]
