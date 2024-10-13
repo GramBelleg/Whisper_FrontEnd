44aba01da8b9333164f02780affa7406cb047a93
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-13 18:09:46 +0300

    Refactor LastMessage to Separate Components.

--- whisper-front/src/components/LastMessage/LastMessage.jsx
+++ whisper-front/src/components/LastMessage/LastMessage.jsx
@@ -1,83 +1,36 @@
 import React, { useEffect, useRef, useState } from "react";
 import "./LastMessage.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMicrophone , faVideo, faCamera, faNoteSticky } from  '@fortawesome/free-solid-svg-icons';
+import TextMessage from "../TextMessage/TextMessage";
+import ImageMessage from "../ImageMessage/ImageMessage";
+import StickerMessage from "../StickerMessage/StickerMessage";
+import VideoMessage from "../VideoMessage/VideoMessage";
+import AudioVoiceNoteMessage from "../AudioVoiceNoteMessage/AudioVoiceNoteMessage";
 const LastMessage = ({ messageType, message, messageState,  index }) => {
-  const [audioDuration, setAudioDuration] = useState(null);
-  const [isTextOverflowing, setIsTextOverflowing] = useState(false);
-  const textRef = useRef(null);
-  const audioRef = useRef(null);
-
-console.log(messageState)
-  useEffect(() => {
-    const checkTextOverflow = () => {
-      if (textRef.current) {
-        const { scrollWidth, clientWidth } = textRef.current;
-        setIsTextOverflowing(scrollWidth > clientWidth);
-      } 
-    };
-
-    checkTextOverflow();
-    window.addEventListener("resize", checkTextOverflow);
-
-    
-    if (audioRef.current && (messageType === "audio" || messageType === "voiceNote")) {
-      const handleLoadedMetadata = () => {
-        setAudioDuration(audioRef.current.duration.toFixed(1));
-      };
-
-      audioRef.current.addEventListener('loadedmetadata', handleLoadedMetadata);
-
-      return () => {
-        window.removeEventListener("resize", checkTextOverflow);
-        if (audioRef.current) {
-          audioRef.current.removeEventListener('loadedmetadata', handleLoadedMetadata);
-        }
-      };
-    }
-  }, [messageType, message]);
+  
 
   const renderMessageContent = () => {
     switch (messageType) {
       case "text":
         return (
-          <p ref={textRef} className={`text-message ${isTextOverflowing ? 'overflow' : ''} ${index ? 'hovered' : ''}`}>
-            {message}
-          </p>
+          <TextMessage index={index} message={message}/>
         );
       case "image":
         return (
-          <div className="image-message">
-            <FontAwesomeIcon icon={faCamera} className={`image-icon ${messageState === 2  ? 'active' : ''}`} />
-            <span className={`image-text ${messageState === 2  ? 'active' : ''}`}>Image</span>
-          </div>
+          <ImageMessage messageState={messageState}/>
         );
       case "audio":
       case "voiceNote":
         return (
-          <div className={`${messageType === "audio" ? "audio-message" : "voice-note-message"}`}>
-            <audio ref={audioRef} controls className="audio-itself">
-              <source src={message} type="audio/mpeg" />
-              Your browser does not support the audio element.
-            </audio>
-            
-            <FontAwesomeIcon icon={faMicrophone} className={`mic-icon ${messageState === 2  ? 'active' : ''}`} />
-            {audioDuration && <span className={`audio-duration ${messageState === 2  ? 'active' : ''}`}>{audioDuration}s</span>}
-          </div>
+          <AudioVoiceNoteMessage messageType={messageType} messageState={messageState} message={message}/>
         );
       case "video":
         return (
-          <div className="video-message">
-            <FontAwesomeIcon icon={faVideo} className={`video-icon ${messageState === 2  ? 'active' : ''}`} />
-            <span className={`video-text ${messageState === 2  ? 'active' : ''}`}>Video</span>
-          </div>
+          <VideoMessage messageState={messageState}/>
         );
       case "sticker":
         return (
-          <div className="sticker-message">
-            <FontAwesomeIcon icon={faNoteSticky} className={`sticker-icon ${messageState === 2  ? 'active' : ''}`}/>
-            <span className={`sticker-text ${messageState === 2  ? 'active' : ''}`}>Sticker</span>
-          </div>
+          <StickerMessage messageState={messageState}/>
         );
       default:
         return null;
