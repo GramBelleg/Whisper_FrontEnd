fb333ab76325504ff7641aec0092b86e07380ed5
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-15 18:35:46 +0300

    Reafctor Info to show drop down menu top at the end

--- whisper-front/src/components/Info/Info.jsx
+++ whisper-front/src/components/Info/Info.jsx
@@ -1,7 +1,7 @@
 // This Shows the Icon of dropdown menu
 // You can then mute chats
 
-import React, { useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./Info.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
@@ -10,7 +10,10 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 const Info = ({ index, group }) => {
 
-    // This  is the state for the dropdown menu
+    const infoRef = useRef(null); 
+    const dropdownRef = useRef(null); 
+    const [dropdownPosition, setDropdownPosition] = useState("down");
+
     const [isVisible, setIsVisible] = useState(false);
 
     // This controls the visibity of the drop down
@@ -18,22 +21,56 @@ const Info = ({ index, group }) => {
         setIsVisible(!isVisible);
     };
 
-    // This will be one for every action
+    // TODO: This will be one for every action
     const handleAction = (action) => {
         console.log(`${action} clicked`);
-        // Handle the action here (Mute, Pin, Block, Archive, Leave)
+        // TODO: Handle the action here (Mute, Pin, Block, Archive, Leave)
     };
+
+    // Handle positioning the dropdown based on available space
+    const handlePositioning = () => {
+        const infoRect = infoRef.current.getBoundingClientRect();
+        const dropdownHeight = dropdownRef.current ? dropdownRef.current.offsetHeight : 0;
+        const spaceBelow = window.innerHeight - infoRect.bottom;
+        const spaceAbove = infoRect.top;
+
+        if (spaceBelow < dropdownHeight && spaceAbove >= dropdownHeight) {
+        setDropdownPosition("up"); // Position dropdown above
+        } else {
+        setDropdownPosition("down"); // Position dropdown below
+        }
+    };
+
+    // Recalculate dropdown position when visible or window is resized
+    useEffect(() => {
+        if (isVisible) {
+            handlePositioning();
+        }
+        const handleResize = () => {
+            if (isVisible)
+                handlePositioning();
+        };
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [isVisible]);
     
 
     return (
         <>
         {index && (
-            <div className="info" onClick={toggleDropdown}>
-            <FontAwesomeIcon icon={faChevronDown} style={{color:"grey"}}/>
+            <div className="info" ref={infoRef} onClick={toggleDropdown}>  
+                <FontAwesomeIcon icon={faChevronDown} style={{color:"grey"}}/>
             {isVisible && (
                 <div
                 className="dropdown" // Add a class for dropdown styling
                 onMouseLeave={() => {setIsVisible(false)}}
+                ref={dropdownRef}
+                style={{
+                    top: dropdownPosition === "down" ? "100%" : "auto",
+                    bottom: dropdownPosition === "up" ? "100%" : "auto",
+                }}
                 >
                 <ul >
                     <li
