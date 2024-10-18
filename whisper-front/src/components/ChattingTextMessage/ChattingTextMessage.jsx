af7ea260133e7d679a6d977cae30656644c215cc
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-18 11:48:08 +0300

    👕 Refactor only for cleaning

--- whisper-front/src/components/ChattingTextMessage/ChattingTextMessage.jsx
+++ whisper-front/src/components/ChattingTextMessage/ChattingTextMessage.jsx
@@ -5,45 +5,12 @@ import DeliveredTicks from "../DeliveredTicks/DeliveredTicks";
 import ReadTicks from "../ReadTicks/ReadTicks";
 import { mapMessageState } from "../../services/chatservice/mapMessageState";
 import { useEffect, useState } from "react";
+import PendingSend from "../PendingSend/PendingSend";
 
 const ChattingTextMessage = ({ key, message }) => {
-    /*
-        <div className='message sender shadow'>
-            <div className='message-text'>
-                I plan to go to Norway, Tom said that you can tell about interesting places. I am very interested in the city of
-                Stavanger. Have you been to this city?
-            </div>
-            <div className='message-info'>
-                <span className='time'>12:51</span>
-                <span className='message-status'>
-                    <SentTicks width='12px' />
-                </span>
-            </div>
-        </div>
 
-        <div className='message receiver shadow'>
-            <div className='message-text'>
-                I plan to go to Norway, Tom said that you can tell about interesting places. I am very interested in the city of
-                Stavanger. Have you been to this city?
-            </div>
-            <div className='message-info'>
-                <span className='time'>12:51</span>
-            </div>
-        </div>
-    */
-    /*
-        {
-            content: "I plan to go to Norway, Tom said that you can tell about interesting places. I am very interested in the city of Stavanger. Have you been to this city?",
-            chatId: 3,
-            type: "text",
-            forwarded: false,
-            selfDestruct: true,
-            expiresAfter: 5,
-            parentMessageId: null,
-            sender: 1
-        }
-    */
-    const [myMessage, setMyMessage] = useState({})
+    const [myMessage, setMyMessage] = useState({});
+
     useEffect(() => {
         setMyMessage(
             (prevMessage) => ({
@@ -65,22 +32,29 @@ const ChattingTextMessage = ({ key, message }) => {
                     <span className="time">
                         {myMessage.time}
                     </span>
-                    <span className='message-status'>
                     {
-                        myMessage.state === 0  && (
-                            <SentTicks width='12px'/>
-                        ) 
-                        || 
-                        myMessage.state === 1 && (
-                            <DeliveredTicks width='12px'/>
-                        )
-                        || 
-                        myMessage.state === 2 && (
-                            <ReadTicks  width='12px'/>
-                        )
-                        
-                    } 
-                    </span>
+                        myMessage.sender === 1 && 
+                        <span className='message-status'>
+                        {
+                            myMessage.state === 0  && (
+                                <SentTicks width='12px'/>
+                            ) 
+                            || 
+                            myMessage.state === 1 && (
+                                <DeliveredTicks width='12px'/>
+                            )
+                            || 
+                            myMessage.state === 2 && (
+                                <ReadTicks width='12px'/>
+                            )
+                            || 
+                            myMessage.state === 4 && (
+                                <PendingSend width='12px'/>
+                            )
+                        } 
+                        </span>
+                    }
+                    
                 </div>
             </div>
         </>
