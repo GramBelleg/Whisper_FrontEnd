13e619e1757f05f707c7ecd5a7a10a7077f7a571
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-18 09:57:07 +0300

    ⚡ Update delivered and read ticks to be icons

--- whisper-front/src/components/DeliveredTicks/DeliveredTicks.jsx
+++ whisper-front/src/components/DeliveredTicks/DeliveredTicks.jsx
@@ -1,22 +1,15 @@
-// This is the SVG of the delivered tick
-// It displays the double ticks of delivered messages
+// This is the SVG of the sent tick
+// It displays the single tick to be use by chats
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckDouble } from "@fortawesome/free-solid-svg-icons";
+import "./DeliveredTicks.css"
 
-const DeliveredTicks = () => {
-    return (
-        <div className='delivered-ticks'>
-            <svg
-                fill='none'
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 12 12'
-                width='50%'
-                height='50%'
-                preserveAspectRatio='xMidYMid meet'
-            >
-                <path d='M1 3.55102L3.78226 6L8.5 1' stroke='white' stroke-linecap='round' stroke-linejoin='round' />
-                <path d='M6 3.55102L8.78226 6L13.5 1' stroke='white' stroke-linecap='round' stroke-linejoin='round' />
-            </svg>
+const SentTicks = () => {
+    return ( 
+        <div className="delivered-ticks">
+            <FontAwesomeIcon icon={faCheckDouble} className="delivered-icon"/>
         </div>
-    )
+    );
 }
 
-export default DeliveredTicks
+export default SentTicks
