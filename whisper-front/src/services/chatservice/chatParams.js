e21df755c667f8873492e5cb81ff3b08c5d087f8
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-20 23:35:23 +0300

    🚨 Remove chatParams and isValidChat

--- whisper-front/src/services/chatservice/chatParams.js
+++
@@ -1,4 +0,0 @@
-// Define the required parameters for a valid chat
-export const requiredChatParams = ["name", "profile_pic", "message", "message_state", "muted", "last_seen", "unread_notifications", "message_time", "message_type","tagged","sender", "group", "story"]
-
-export const whoAmI = "John";
--- whisper-front/src/services/chatservice/isValidChat.js
+++
@@ -1,5 +0,0 @@
-import { requiredChatParams } from "./chatParams.js"
-
-export const isValidChat = (chat) => {
-    return chat && typeof chat === 'object' && requiredChatParams.every((param) => param in chat)
-}
