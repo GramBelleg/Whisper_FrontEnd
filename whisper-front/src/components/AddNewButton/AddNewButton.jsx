060bee713ab017054da550dcb531ed9cb037de58
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-21 19:41:33 +0300

    ⭐ Add new button for creating groups and channels later.

---
+++ whisper-front/src/components/AddNewButton/AddNewButton.jsx
@@ -0,0 +1,14 @@
+import "./AddNewButton.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencil } from "@fortawesome/free-solid-svg-icons";
+
+
+const AddNewButton = ({ onClick }) => {
+    return (
+        <button className="add-new-button" onClick={onClick}>
+            <FontAwesomeIcon icon={faPencil} />
+        </button>
+    );
+};
+
+export default AddNewButton;
