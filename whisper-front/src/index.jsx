2897888d7166fd3bef8c805d3cbfd3347f5cd8c9
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-23 21:13:26 +0300

    ⚡ Update some files

--- whisper-front/src/index.jsx
+++ whisper-front/src/index.jsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/main.css'
 import './styles/colors.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {AuthProvider} from './contexts/AuthContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <App />
+        <AuthProvider>
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+                <App />
+            </GoogleOAuthProvider>
+        </AuthProvider>
     </StrictMode>
 )
