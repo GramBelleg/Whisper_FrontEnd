cefa0cbd6fe74b51d979ca6f9d7c298b9f59de86
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-10-26 11:01:31 +0300

    💥 Update for deadcode in SingleChatSection

--- whisper-front/src/components/SingleChatSection/SingleChatSection.jsx
+++ whisper-front/src/components/SingleChatSection/SingleChatSection.jsx
@@ -95,9 +95,10 @@ const SingleChatSection = ({ selectedUser }) => {
         else
         {
             console.log(`file uploaded successfully`);
+            tempMessageObject.blobName = blobName;
             return { blobName };
         }
-        tempMessageObject.blobName = blobName;
+        
     }
     const formatFileName = (fileName, length) => {
         if (fileName.length > 20) {
