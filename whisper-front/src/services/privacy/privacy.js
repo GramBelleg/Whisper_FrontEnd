62755ed8aaa9621b256b110bd4261698f0762add
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-11-08 20:52:43 +0200

    💥 Update API calls

--- whisper-front/src/services/privacy/privacy.js
+++ whisper-front/src/services/privacy/privacy.js
@@ -34,6 +34,22 @@ export const putLastSeenVisibilitySettings = async (setting) => {
     }
 }
 
+export const putStoriesVisibilitySettings = async (setting) => {
+    try {
+        const response = await axios.put('http://localhost:5000/api/user/story/privacy', 
+            { "privacy": setting },
+            { withCredentials: true }
+        );
+        
+
+        console.log('Response:', response.data);
+        return response.data;
+    
+    } catch (error) {
+        throw error;
+    }
+}
+
 export const putProfilePicVisibilitySettings = async (setting) => {
     try {
         const response = await axios.put('http://localhost:5000/api/user/pfp/privacy', 
--- whisper-front/src/services/storiesservice/setStoryVisibility.js
+++ whisper-front/src/services/storiesservice/setStoryVisibility.js
@@ -1,25 +1,17 @@
 import axios from "axios";
-import axiosInstance from "../axiosInstance"
-
-
 
 export const setStoryPrivacySettings = async (storyId, privacy) => {
 
     try {
-        console.log("Hello")
-        const data = await axios.post('http://localhost:5000/api/user/story/privacy', 
+        const response = await axios.put(`http://localhost:5000/api/user/story/${storyId}/privacy`, 
             { 
-                "storyId": storyId,
                 "privacy": privacy
-            }
-            ,
-            {
-                withCredentials: true
-            }
+            },
+            { withCredentials: true }
+            
         );
-
-        console.log("Changed Visibility")
-
+        
+        console.log(response)
     } catch (err) {
         throw err;
     }
