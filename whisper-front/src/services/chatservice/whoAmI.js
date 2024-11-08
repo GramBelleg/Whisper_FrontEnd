89fdd4c43662c1e40780d0b923b77db4e758093e
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-11-08 20:53:20 +0200

    💥 Update whoAmI object

--- whisper-front/src/services/chatservice/whoAmI.js
+++ whisper-front/src/services/chatservice/whoAmI.js
@@ -2,8 +2,8 @@ import noUser from '../../assets/images/no-user.png'
 
 export let whoAmI =  {
     profilePic : noUser,
-    storySettings: "Everyone",
-    profileVisibility: "Everyone",
-    lastSeenVisibility: "Everyone",
+    storyPrivacy: "Everyone",
+    pfpPrivacy: "Everyone",
+    lastSeenPrivacy: "Everyone",
     readReceipts: true
 };
