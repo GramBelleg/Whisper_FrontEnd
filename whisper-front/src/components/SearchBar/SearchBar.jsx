554a408fc47dbb4cae52555f7671ea28692bfc0d
hanamostafaa <hmostafa935@gmail.com>
2024-11-24 00:04:26 +0200

    ⚡Update Search in chat

--- whisper-front/src/components/SearchBar/SearchBar.jsx
+++ whisper-front/src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Search from '../../assets/images/search.svg?react';
 import './SearchBar.css'; 
-const SearchBar = ({handleSearchClick}) => {
+const SearchBar = ({handleQueryChange,handleSearchClick}) => {
   
     return (
       <div className="search-bar">
         <Search data-testid="search-bar-test" className="search-icon cursor-pointer" onClick={handleSearchClick}/>
-        <input type="text" placeholder="Search" data-testid="search-input-test" className="search-input" />
+        <input type="text" placeholder="Search" data-testid="search-input-test" className="search-input" onChange={handleQueryChange} />
       </div>
     );
   };
---
+++ whisper-front/src/services/chatservice/searchChat.js
@@ -0,0 +1,36 @@
+import axiosInstance from "../axiosInstance";
+export const handleSearchChat = async ( query, chatId ) => {
+    try
+    {
+    // const response = await axiosInstance.get(
+    //     `/api/messages/${chatId}/searchMessages`, 
+    //     {
+    //       params: { query: query }, 
+    //     }
+    //   );
+      //return response.data;
+      const mockData = [
+        {
+          id: 1,
+          chatId: 1,
+          sender: "Alice",
+          content: "Hello, Bob!",
+          sentAt: "2022-01-01T12:00:00Z",
+          isPinned: false,
+        },
+        {
+          id: 2,
+          chatId: 1,
+          sender: "Bob",
+          content: "Hi, Alice!",
+          sentAt: "2022-01-01T12:01:00Z",
+          isPinned: false,
+        },
+      ];
+      return mockData;
+    }
+    catch(error)
+    {
+      console.error("Error searching chat:", error);
+    }
+}
