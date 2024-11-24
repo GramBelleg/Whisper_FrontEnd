90d673e9e14b6e27076701dfeec5c607884214b6
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-11-24 20:12:44 +0200

    👕 Refactor SingleChatSection

--- whisper-front/src/components/SingleChatSection/SingleChatSection.jsx
+++ whisper-front/src/components/SingleChatSection/SingleChatSection.jsx
@@ -6,21 +6,19 @@ import ChatActions from '../ChatActions/ChatActions'
 import { useChat } from '@/contexts/ChatContext'
 import NoChatOpened from '../NoChatOpened/NoChatOpened'
 import PinnedMessages from '../PinnedMessages/PinnedMessages'
-import { useEffect, useState } from 'react'
 import SearchSingleChat from '../SearchSingleChat/SearchSingleChat'
+import { useEffect } from 'react'
 
 const SingleChatSection = () => {
     const { currentChat, pinnedMessages } = useChat();
 
-
     if (!currentChat) {
         return (<NoChatOpened />);
     }
-    const [myPinnedMessages, setMyPinnedMessages] = useState(pinnedMessages);
-
     useEffect(() => {
-        setMyPinnedMessages(pinnedMessages);
-    }, [pinnedMessages]);
+
+    }, [pinnedMessages])
+    
     return (
         <div className='single-chat-container'>
             <div className='single-chat-header shadow-md'>
@@ -40,10 +38,9 @@ const SingleChatSection = () => {
             <div className='messages'>
                 <SingleChatMessagesList/>
                 {
-                    myPinnedMessages.length > 0 && (
+                    pinnedMessages.length > 0 && (
                         <div>
                             <PinnedMessages
-                                pinnedMessages={myPinnedMessages}
                                 onGoToMessage={(message) => console.log("Go to message", message)}
                             />
                         </div>
