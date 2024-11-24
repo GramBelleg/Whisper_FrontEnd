d61929151f040ac17851ff9bf85435ba5b9f0730
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-11-24 20:12:16 +0200

    ⚡ Update for removing states

--- whisper-front/src/components/SingleChatMessagesList/SingleChatMessagesList.jsx
+++ whisper-front/src/components/SingleChatMessagesList/SingleChatMessagesList.jsx
@@ -2,22 +2,19 @@ import ChatMessage from "../ChatMessage/ChatMessage";
 import parentRelationshipTypes from "../../services/chatservice/parentRelationshipTypes";
 import { useChat } from '@/contexts/ChatContext';
 import "./SingleChatMessagesList.css";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 
 const SingleChatMessagesList = () => {
     const { messages, updateParentMessage } = useChat();
 
-    const [myMessages, setMyMessages] = useState([]);
-
     useEffect(() => {
-        setMyMessages(messages);
     }, [messages]);
 
     return (
         <div className="single-chat-messages-list">
             {
-                myMessages?.map((message,index) => (
+                messages?.map((message,index) => (
                     <ChatMessage
                         key={index}
                         onReply={() => { updateParentMessage(message, parentRelationshipTypes.REPLY); }}
