6aaf3635fea58e8f3c376a0de53f5fc986e6f2f4
Fatmaz04 <118017527+Fatmaz04@users.noreply.github.com>
2024-12-01 23:05:12 +0200

    ⚡ Update Side bar to be scrollable

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -7,7 +7,6 @@ import BlockedUsers from "../BlockedUsers/BlockedUsers";
 import VisibilitySettings from "../VisibiltySettings/VisibilitySettings";
 import SettingsPage from "@/pages/SettingsPage";
 
-
 const pages = [
     {
       id: 'chat',
@@ -36,55 +35,21 @@ const Sidebar = () => {
     const { activePage } = useSidebar();
     const [sidebarWidth, setSidebarWidth] = useState(30); 
     const sidebarRef = useRef(null);
-    const isResizing = useRef(false);
-
-    const startResizing = () => {
-        isResizing.current = true;
-    };
-
-    const stopResizing = () => {
-        isResizing.current = false;
-    };
-
-    const resize = (e) => {
-        if (isResizing.current) {
-            const newWidth = (e.clientX / window.innerWidth) * 100; 
-            if (newWidth >= 20 && newWidth <= 45) {
-                setSidebarWidth(newWidth);
-            }
-        }
-    };
 
     const currentPage = pages.find(page => page.id === activePage) || pages[0];
     const Component = currentPage.Component;
-
-
-    useEffect(() => {
-        const handleMouseUp = stopResizing;
-        const handleMouseMove = resize;
-
-        document.addEventListener('mousemove', handleMouseMove);
-        document.addEventListener('mouseup', handleMouseUp);
-
-        return () => {
-            document.removeEventListener('mousemove', handleMouseMove);
-            document.removeEventListener('mouseup', handleMouseUp);
-        };
-    }, []);
 
     return (
         <div
             className="sidebar"
             ref={sidebarRef}
-            style={{ width: `${sidebarWidth}%` }}
+            style={{ width: `${sidebarWidth}%`}}
         >
-            <Component />
-            <div
-                className="sidebar__resizer"
-                onMouseDown={startResizing}
-            />
+            <div className="sidebar__content">
+                <Component />
+            </div>
         </div>
     )
 }
 
-export default Sidebar
+export default Sidebar;
