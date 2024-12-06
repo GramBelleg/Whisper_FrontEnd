b1c3a58664522f302ca402e6db7c322024b88139
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-12-06 10:06:08 +0200

    💥 Update config to correct url

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const apiUrl = import.meta.env.NODE_ENV === 'development'
+const apiUrl = process.env.NODE_ENV === 'development'
     ? import.meta.env.VITE_APP_API_URL
     : import.meta.env.VITE_APP_SERVER_URL;
-
+console.log(apiUrl)
 export default apiUrl;
