305f8598486ab3f28fbd7b01ec702f08f2f6943c
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-12-07 14:10:18 +0200

    ⭐ Add .env.production

--- src/config.js
+++ src/config.js
@@ -1,5 +1,3 @@
-const apiUrl = process.env.NODE_ENV === 'development'
-    ? import.meta.env.VITE_APP_API_URL
-    : import.meta.env.VITE_APP_SERVER_URL;
+const apiUrl = "https://whisper.webredirect.org"
 console.log("Api url", apiUrl)
 export default apiUrl;
