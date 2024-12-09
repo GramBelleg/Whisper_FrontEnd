13af56871a65ac786ef87bbc709d4caee26d9a62
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-12-09 19:44:25 +0200

    ⭐ Add get chat members call

---
+++ src/services/chatservice/getChatMembers.js
@@ -0,0 +1,21 @@
+import apiUrl from "@/config";
+import axios from "axios";
+
+
+export const getMembers = async (chatId) => {
+    try {
+        const tokenFromCookies = localStorage.getItem('token')
+        const response = await axios.get(`${apiUrl}/api/chats/${chatId}/getMembers`,
+            { withCredentials: true }, {
+                headers: {
+                    Authorization: `Bearer ${tokenFromCookies}`
+                }
+            }
+        )
+        console.log(response)
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
