77bd99476c4aed3d3b6db48e6d9da4fac9717fff
zizoisprogramming <ziadsamerbarca@gmail.com>
2024-12-13 17:44:26 +0200

    ⭐ Add AddAmdin and remove user functionality

--- src/components/GroupMembers/GroupMembers.jsx
+++ src/components/GroupMembers/GroupMembers.jsx
@@ -1,23 +1,92 @@
-import SearchBar from '../SearchBar/SearchBar'
-import noUser from '../../assets/images/no-user.png'
-const GroupMembers = ({ filteredMembers, handleQueryChange }) => {
+import React, { useRef, useEffect, useState } from 'react';
+import SearchBar from '../SearchBar/SearchBar';
+import noUser from '../../assets/images/no-user.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import useAuth from '@/hooks/useAuth';
+
+const GroupMembers = ({ filteredMembers, handleQueryChange, amIAdmin, handleAddAmin, handleRemoveFromChat }) => {
+    const [menuState, setMenuState] = useState({
+        isVisible: false,
+        selectedUser: null, 
+    })
+    const { user } = useAuth()
+
+    const menuRef = useRef(null)
+
+    const handleChevronClick = (event, member) => {
+        event.stopPropagation(); 
+
+        setMenuState({
+            isVisible: !menuState.isVisible,
+            selectedUser: member,  
+        })
+    }
+
+    const handleCloseMenu = () => {
+        setMenuState({ ...menuState, isVisible: false });
+    }
+
+    useEffect(() => {
+        
+        const handleClickOutside = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setMenuState({ ...menuState, isVisible: false });
+            }
+        }
+
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, [menuState])
+
     return (
-        <div className=' p-4 rounded-md'>
+        <div className='p-4 rounded-md' onClick={handleCloseMenu}>
             <h2 className='text-lg text-light text-left mb-6'>Group Members</h2>
             <SearchBar handleQueryChange={handleQueryChange} className='pd-4' />
 
             <div className='members-list mg-4'>
-                {filteredMembers?.map((member) => (
-                    <div key={member.id} className='user-item'>
+                {filteredMembers?.map((member, index) => (
+                    <div
+                        key={member.id}
+                        className='user-item'
+                        style={{ position: 'relative' }} 
+                    >
                         <div className='user-image'>
                             <img src={member.profilePic || noUser} alt={member.userName} />
                         </div>
                         <label htmlFor={`user-${member.id}`}>{member.userName || 'Unknown User'}</label>
+
+                        {   
+                            amIAdmin && !member.isAdmin && member.id !== user.id && <FontAwesomeIcon
+                                icon={faChevronDown}
+                                className='chevron-icon'
+                                onClick={(e) => handleChevronClick(e, member)} 
+                                style={{ 
+                                    cursor: 'pointer',
+                                    marginLeft: "2%",
+                                }}
+                            />
+                        }
+
+                        {menuState.isVisible && menuState.selectedUser === member && (
+                            <div
+                                className="absolute top-full left-0 bg-[#081d3c] shadow-lg rounded-md z-50 mt-1 cursor-pointer text-white transition-all duration-300 ease-in-out"
+                                ref={menuRef}
+                            >
+                                <p className='w-full rounded-md hover:bg-[var(--accent-color)] hover:shadow-xl p-2' onClick={() => handleAddAmin(member.id)}>Promote to admin</p>
+                                <p className='w-full rounded-md hover:bg-[var(--accent-color)] hover:shadow-xl p-2' onClick={() => handleRemoveFromChat(member)}>Remove from group</p>
+
+                            </div>
+                        )}
+
                     </div>
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default GroupMembers
+export default GroupMembers;
