04c12cc2e21e5a439ecc3a0c3cf27f8b4a65a8fa
Fatmaz04 <118017527+Fatmaz04@users.noreply.github.com>
2024-12-21 19:36:42 +0200

    💄 Updating Notification Style

--- src/components/NotificationMessage/NotificationMessage.jsx
+++ src/components/NotificationMessage/NotificationMessage.jsx
@@ -1,13 +1,14 @@
 const NotificationMessage = ({ notification }) => {
-  console.log(notification)
+    console.log(notification)
+    const truncateText = (text, maxLength) => (text.length > maxLength ? text.slice(0, maxLength) + '...' : text)
     return (
-      <>
-        <div id="notificationHeader">
-          <h3 className="font-bold">{notification.title}:</h3>
+        <div className='flex flex-col text-left'>
+            <div id='notificationHeader'>
+                <h3 className='font-bold'>{notification.title}</h3>
+            </div>
+            <div id='notificationBody'>{truncateText(notification.body, 100)}</div>
         </div>
-        <div id="notificationBody">{notification.body}</div>
-      </>
-    );
-  };
-  
-  export default NotificationMessage;
+    )
+}
+
+export default NotificationMessage
